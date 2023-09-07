d673be78e38df8b9f5f1ce8191e7d51d3909d1b8
88samah <88samcha@gafe.molndal.se>
2023-09-07 23:25:02 +0200

    klar

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,16 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        double celsius= 25;//temperatur i celsius
+        double fahrenheit= (celsius * 1.8) + 32;//omvandla celsius till fahrenheit
+        double kelvin= celsius + 273.15; // omvandla celsius till kelvin
+
+        // utskrift
+
+        System.out.println("Celsius: " + celsius + " grader");
+        System.out.println("Fahrenheit: " + fahrenheit+" grader");
+        System.out.println("Kelvin:  " + kelvin + " grader");
+
+
     }
 }
